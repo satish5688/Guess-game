07a07673df01c366871fdec0618dd977c4c4c2ff
Toliram Mungusmare <satish.mungusmare@accenture.com>
2023-08-09 12:29:10 +0530

    added PlayAgain fuctinality

--- index.js
+++ index.js
@@ -1,51 +1,54 @@
-let Random_number = parseInt(Math.floor(Math.random() * 20 + 1));
-let score=20
-function check_number(){
-    let result;
-    let number = parseInt(document.getElementById('guessed-number').value);
-    // switch(number){
-    //     case number===Random_number:
-    //         result= 'Correct Number';
-    //         console.log(number);
-    //         break;
-    //     case number>Random_number:
-    //         result= 'Too High';
-    //         break;
-    //     case number<Random_number:
-    //         result = 'Too Low';
-    //         break;
-    //     default:
-    //         result = 'Something Unexpected';
-    //         console.log(number);
-    //         break;
+var Random_number = parseInt(Math.floor(Math.random() * 20 + 1));
 
-    // }
-    if (number==Random_number){
-        result = 'Correct number'
-        document.getElementById('highscore').innerHTML=`HighScore: ${score}`
-        document.body.style.backgroundColor = "lightgreen"
-        var element=document.getElementById('correct-number')
-        element.innerHTML=number
-        element.style.color='black'
-        element.style.fontWeight='bold'
-        element.style.fontSize='2rem'
-    }else if(number>Random_number) {
-        result = 'Too High'
-        score-=1
+function play_again() {
+  Random_number = parseInt(Math.floor(Math.random() * 20 + 1));
+  console.log(Random_number);
+  document.getElementById("status").innerHTML = 'Start Guessing';
+  document.getElementById("guessed-number").value = 0;
+  var correct = document.getElementById("correct-number").innerHTML.trim();
+
+  document.getElementById("check").disabled = false;
+  let score = document.getElementById("score");
+  let highscore = document.getElementById("highscore");
+  // console.log(Random_number, score.innerHTML, highscore.innerHTML,correct, 'hu.....');
+  if (
+    parseInt(highscore.innerHTML) < parseInt(score.innerHTML) &&
+    correct !== "?"
+  ) {
+    highscore.innerHTML = score.innerHTML;
+  }
+  score.innerHTML = 20;
+  document.body.style.backgroundColor = "black";
+  document.getElementById("correct-number").innerHTML = "?";
+}
+
+
+
+function check_number() {
+  let score = parseInt(document.getElementById("score").innerHTML);
+  let result;
+  let number = parseInt(document.getElementById("guessed-number").value);
+  if (score <= 0) {
+    result = "You loose all your chances";
+    document.getElementById("check").disabled = true;
+  } else {
+    if (number == Random_number) {
+      result = "Correct number";
+      document.body.style.backgroundColor = "lightgreen";
+      document.getElementById("check").disabled = true;
+      var element = document.getElementById("correct-number");
+      element.innerHTML = number;
+    } else if (number > Random_number) {
+      result = "Too High";
+      score -= 1;
+    } else if (number < Random_number) {
+      result = "Too low";
+      score -= 1;
+    } else {
+      result = "something unexpected";
     }
-    else if (number<Random_number){
-        result= 'Too low'
-        score-=1
-    }
-    else{
-        result= 'something unexpected'
-    }
-
-    console.log(result);
-
-    document.getElementById('status').innerHTML=result
-    document.getElementById('score').innerHTML= `Score: ${score}`
-    
-
+  }
+  document.getElementById("status").innerHTML = result;
+  document.getElementById("score").innerHTML = score;
 }
 console.log(Random_number);
